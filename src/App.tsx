4aab7d8a1bcb56c9aaeaa16216debf9c83b4588a
unknown <joshuafrellt@gmail.com>
2025-12-24 16:51:11 +0800

    feat: dynamic background color

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,16 @@ function App() {
   useEffect(() => {
     if (!isRunning) return
 
-    document.body.className = bgColor 
-    
     const intervalId = setInterval(() => {
       setTimeLeft(t => t - 1)
     }, 1000)
 
     return () => clearInterval(intervalId)
   }, [isRunning])
+
+  useEffect(() => {
+    document.body.className = bgColor 
+  }, [bgColor])
 
 
   function formatTime() {
@@ -27,19 +29,19 @@ function App() {
   
 
   return (
-    <div className="bg-gray-50 rounded-2xl shadow-xl w-[360px] sm:w-[500px] h-[250px] sm:h-[325px] absolute top-1/2 left-1/2 -translate-1/2 flex flex-col justify-center items-center gap-3">
+    <div className="bg-gray-50 rounded-2xl shadow-xl w-[360px] sm:w-[500px] h-[250px] sm:h-[325px] absolute top-1/2 left-1/2 -translate-1/2 flex flex-col justify-center items-center gap-5 sm:gap-7">
       <div className="text-8xl font-semibold sm:text-9xl">
         <span>{formatTime()}</span>
       </div>
-      <div className="flex gap-4">
+      <div className="flex gap-4 sm:gap-6">
         <button 
-          className="bg-green-700 active:bg-green-900 text-white text-2xl px-4 py-1 w-25 rounded-sm cursor-pointer"
+          className="bg-green-700 active:bg-green-900 text-white text-2xl px-4 py-1 w-25 sm:px-5 rounded-sm cursor-pointer"
           onClick={() => setIsRunning(true)}
         >
           Start
         </button>
         <button 
-          className="bg-red-700 active:bg-red-900 text-white text-2xl px-4 py-1 w-25 rounded-sm cursor-pointer"
+          className="bg-red-700 active:bg-red-900 text-white text-2xl px-4 py-1 w-25 sm:px-5 rounded-sm cursor-pointer"
           onClick={() => setIsRunning(false)}
         >
           Pause
